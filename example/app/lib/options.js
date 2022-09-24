f5f4e6ed6ea35dc49ae24b1c45e37ff616140b65
Linh Nguyen <43808684+linhnguyen-gt@users.noreply.github.com>
2022-09-24 15:01:24 +0700

    update

--- example/app/lib/options.js
+++ example/app/lib/options.js
@@ -2,69 +2,45 @@
  * define all valid options
  */
 
- export default [
+export default [
   /**
    * a list of button titles (required)
    * @type string[]
    * @example
    *   ['cancel', 'Apple', 'Banana']
    */
-  "options",
+  'options',
 
   /**
    * index of cancel button in options
    * @type int
    */
-  "cancelButtonIndex",
-
-  /**
-   * the color used for the change the text color of the cancel button
-   * @type string
-   */
-  "cancelButtonTintColor",
+  'cancelButtonIndex',
 
   /**
    * index of destructive button in options
    * @type int
    */
-  "destructiveButtonIndex",
+  'destructiveButtonIndex',
 
   /**
    * a title to show above the action sheet
    * @type string
    */
-  "title",
+  'title',
 
   /**
    * a message to show below the title
    * @type string
    */
-  "message",
-
-  /**
-   * the node to which the action sheet should be anchored (used for iPad)
-   * @type number
-   */
-  "anchor",
+  'message',
 
   /**
    * the color used for non-destructive button titles
    * @type string
    * @see http://facebook.github.io/react-native/docs/colors.html
    */
-  "tintColor",
-
-  /**
-   * a list of button indices which should be disabled
-   * @type array of numbers
-   */
-  "disabledButtonIndices",
-
-  /**
-   * the interface style used for the action sheet, can be set to light or dark, otherwise the default system style will be used
-   * @type string
-   */
-  "userInterfaceStyle",
+  'tintColor',
 
   /**
    * The 'callback' function takes one parameter, the zero-based index of the selected item
@@ -72,5 +48,5 @@
    * @example
    *   (buttonIndex) => if (buttonIndex === 1) { // do something }
    */
-  "onPress",
-];
+  'onPress'
+]
